c7d4523e9a3163d2f8e6108c2cb1cfa1a5ff0049
William Fish <william.michael.fish@gmail.com>
2022-09-07 11:10:39 +0100

    HOTT-1938: Remove dead comment

--- lib/paas_config.rb
+++ lib/paas_config.rb
@@ -1,8 +1,6 @@
 class PaasConfig
   def self.redis
     url = begin
-      # TODO: !Important
-      # need to fetch by service name if we use multiple redis services
       JSON.parse(ENV["VCAP_SERVICES"])["redis"][0]["credentials"]["uri"]
     rescue StandardError
       ENV["REDIS_URL"]
